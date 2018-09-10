9f68a13d690b265d6ac0316191de7724f6216a07
mbenezra18 <michayla.benezra@gmail.com>
2018-09-09 23:21:54 -0400

    Add files via upload

--- TestTriangle.py
+++ TestTriangle.py
@@ -22,7 +22,7 @@ class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
     def testRightTriangleA(self): 
-        self.assertEqual(classifyTriangle(3, 4, 5), 'Right', '3,4,5 is a Right triangle')
+        self.assertEqual(classifyTriangle(3,4,5), 'Right', '3,4,5 is a Right triangle')
 
     def testRightTriangleB(self): 
         self.assertEqual(classifyTriangle(5, 3, 4), 'Right', '5,3,4 is a Right triangle')
@@ -37,7 +37,7 @@ class TestTriangles(unittest.TestCase):
         self.assertEqual(classifyTriangle(13, 14, 9), 'Scalene', '13,14,9 should be scalene')
 
     def testScaleneTrianglesB(self):
-        self.assertEqual(classifyTriangle(13, 14, 9), 'Scalene', '13,14,9 should be scalene')
+        self.assertEqual(classifyTriangle(16, 14, 9), 'Scalene', '13,14,9 should be scalene')
 
     def testIsoscelesTrianglesA(self):
         self.assertEqual(classifyTriangle(4, 4, 1), 'Isosceles', '4,4,1 should be isosceles')
